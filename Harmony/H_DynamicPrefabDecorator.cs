8b4819276b1135373930c4cc5d3c3c40c9cb8af4
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2024-08-23 15:07:38 +0200

    Make tunneling being generated after prefab spawns

--- Harmony/H_DynamicPrefabDecorator.cs
+++ Harmony/H_DynamicPrefabDecorator.cs
@@ -1,12 +1,16 @@
-// using HarmonyLib;
+using HarmonyLib;
 
 
-// [HarmonyPatch(typeof(DynamicPrefabDecorator), "DecorateChunk")]
-// [HarmonyPatch(new[] { typeof(World), typeof(Chunk), typeof(bool) })]
-// public class DynamicPrefabDecorator_DecorateChunk
-// {
-//     public static void Postfix(DynamicPrefabDecorator __instance, Chunk _chunk)
-//     {
-//         // TODO: CaveDecorator.AddDecorationsToCave(_chunk);
-//     }
-// }
+[HarmonyPatch(typeof(DynamicPrefabDecorator), "DecorateChunk")]
+[HarmonyPatch(new[] { typeof(World), typeof(Chunk), typeof(bool) })]
+public class DynamicPrefabDecorator_DecorateChunk
+{
+    // run cave generation after prefabs spawn, to allow caves digging into rwg streetTiles
+    public static void Postfix(DynamicPrefabDecorator __instance, Chunk _chunk)
+    {
+        if (CaveGenerator.isEnabled)
+        {
+            CaveGenerator.GenerateCave(_chunk);
+        }
+    }
+}
