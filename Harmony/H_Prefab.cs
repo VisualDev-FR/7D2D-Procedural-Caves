aae2040378c7b0305485d89f2605d2152eedb38f
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2024-08-26 21:36:35 +0200

    load caveAir in prefabs during runtime

---
+++ Harmony/H_Prefab.cs
@@ -0,0 +1,14 @@
+using HarmonyLib;
+
+
+[HarmonyPatch(typeof(Prefab), "GetBlock")]
+public static class Prefab_GetBlock
+{
+    public static void Postfix(int _x, int _y, int _z, Prefab __instance, ref BlockValue __result)
+    {
+        if (__result.isair && !GameManager.Instance.IsEditMode() && __instance.tags.Test_AnySet(CaveConfig.tagCave))
+        {
+            __result = new BlockValue((uint)Block.GetBlockByName("caveAir").blockID);
+        }
+    }
+}
