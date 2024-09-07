ded426301eb1ada913b11989d6865210f75199e3
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2024-09-07 14:04:00 +0200

    attempting increase terrain height (failed)

--- Harmony/H_WorldBuilder.cs
+++ Harmony/H_WorldBuilder.cs
@@ -48,3 +48,22 @@ public static class WorldBuilder_saveRawHeightmap
         return true;
     }
 }
+
+// [HarmonyPatch(typeof(WorldBuilder), "initStreetTiles")]
+// public static class WorldBuilder_initStreetTiles
+// {
+//     private static readonly int minTerrainHeight = 50;
+
+//     public static bool Prefix(WorldBuilder __instance)
+//     {
+//         for (int i = 0; i < __instance.HeightMap.Length; i++)
+//         {
+//             __instance.HeightMap[i] = minTerrainHeight + (255 - minTerrainHeight) * (__instance.HeightMap[i] / 255);
+//             __instance.terrainDest[i] = (float)(minTerrainHeight + (255 - minTerrainHeight) * (__instance.terrainDest[i] / 255)) / 255;
+//             __instance.terrainWaterDest[i] = (float)(minTerrainHeight + (255 - minTerrainHeight) * (__instance.terrainWaterDest[i] / 255)) / 255;
+//             __instance.waterDest[i] = (float)(minTerrainHeight + (255 - minTerrainHeight) * (__instance.waterDest[i] / 255)) / 255;
+//         }
+
+//         return true;
+//     }
+// }
