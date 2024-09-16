e1f0850c82b568c4a612926b9f9512da2f000258
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2024-09-16 23:20:02 +0200

    improve cellular rooms

--- CaveBuilder/CellularAutomata.cs
+++ CaveBuilder/CellularAutomata.cs
@@ -1,42 +1,44 @@
 using System;
 using System.Collections.Generic;
-using System.Threading;
-
+using System.Linq;
 
 public class CellAut
 {
-    public static Vector3i size = new Vector3i(20, 10, 20);
+    public static int width = 60;
 
-    public static Vector3i Center => size / 2;
+    public static int height = 30;
+
+    public static Vector3i size = new Vector3i(width, width / 3, width / 2);
 
     public static string seed = "";
 
-    public static int randomFillPercent = 60;
+    public static int randomFillPercent = 51;
+
+    public static int passes = 10;
+
+    public static int criteria = 13;
 
     public static Random pseudoRandom;
 
-    public static int[,,] map;
+    public static byte[,,] map;
 
     public static void Execute(string[] args)
     {
-        var width = 50;
-        var height = 50;
+        long memoryBefore = GC.GetTotalMemory(true);
 
-        size = new Vector3i(width, height, width);
-        map = new int[size.x, size.y, size.z];
+        map = new byte[size.x, size.y, size.z];
         pseudoRandom = InitRandom();
 
         var timer = CaveUtils.StartTimer();
 
         RandomFillMap();
 
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < passes; i++)
         {
             SmoothMap();
         }
 
         var voxels = new HashSet<Voxell>();
-
         for (int x = 0; x < size.x; x++)
         {
             for (int y = 0; y < size.y; y++)
@@ -51,7 +53,7 @@ public class CellAut
             }
         }
 
-        Log.Out($"{voxels.Count} blocks, timer: {timer.ElapsedMilliseconds} ms");
+        Log.Out($"{voxels.Count} blocks, timer: {timer.ElapsedMilliseconds} ms, memory: {(GC.GetTotalMemory(true) - memoryBefore) / 1_048_000:F0}MB");
 
         CaveViewer.GenerateObjFile("cellular.obj", voxels);
     }
@@ -65,18 +67,22 @@ public class CellAut
             iSeed = DateTime.Now.GetHashCode();
         }
 
+        CaveNoise.pathingNoise.SetSeed(iSeed);
+
         return new Random(iSeed); ;
     }
 
     public static void RandomFillMap()
     {
+        var sqrRadius = (width >> 1) * (width >> 1);
+
         for (int x = 1; x < size.x - 1; x++)
         {
             for (int y = 1; y < size.y - 1; y++)
             {
                 for (int z = 1; z < size.z - 1; z++)
                 {
-                    map[x, y, z] = pseudoRandom.Next(0, 100) < randomFillPercent ? 1 : 0;
+                    map[x, y, z] = (byte)(pseudoRandom.Next(0, 100) < randomFillPercent ? 1 : 0);
                 }
             }
         }
@@ -84,25 +90,33 @@ public class CellAut
 
     public static void SmoothMap()
     {
+        var newMap = new byte[size.x, size.y, size.z];
+
         for (int x = 1; x < size.x - 1; x++)
         {
-            for (int y = 1; y < size.y - 1; y++)
+            for (int z = 1; z < size.z - 1; z++)
             {
-                for (int z = 1; z < size.z - 1; z++)
+                for (int y = 1; y < size.y - 1; y++)
                 {
                     int neighbourWallTiles = GetNeighborsCount(x, y, z);
 
-                    if (neighbourWallTiles > 13)
+                    if (neighbourWallTiles > criteria)
                     {
-                        map[x, y, z] = 1;
+                        newMap[x, y, z] = 1;
                     }
-                    else if (neighbourWallTiles < 13)
+                    else if (neighbourWallTiles > (criteria - 1))
                     {
-                        map[x, y, z] = 0;
+                        newMap[x, y, z] = (byte)(pseudoRandom.Next(0, 100) < 50 ? 1 : 0);
+                    }
+                    else
+                    {
+                        newMap[x, y, z] = 0;
                     }
                 }
             }
         }
+
+        map = newMap;
     }
 
     public static int GetNeighborsCount(int x, int y, int z)
