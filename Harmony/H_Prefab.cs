b7073097d52a86761b5d7d8fd7168d8122fbc1a5
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2024-10-27 17:59:23 +0100

    remove caveAir in prefabs above terrain height

--- Harmony/H_Prefab.cs
+++ Harmony/H_Prefab.cs
@@ -7,7 +7,12 @@ public static class Prefab_GetBlock
     // NOTE: light patch allowing to replace air by caveAir in cavePrefabs
     public static void Postfix(int _x, int _y, int _z, Prefab __instance, ref BlockValue __result)
     {
-        if (__result.isair && !GameManager.Instance.IsEditMode() && __instance.tags.Test_AnySet(CaveConfig.tagCave))
+        if (
+            __result.isair
+            && !GameManager.Instance.IsEditMode()
+            && __instance.tags.Test_AnySet(CaveConfig.tagCave)
+            && _y >= -__instance.yOffset
+        )
         {
             __result = CaveBlocks.caveAir;
         }
