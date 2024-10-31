747291d345e25d445e8c44355d5ac0a788b17e1f
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2024-10-31 18:03:33 +0100

    setup logging module

---
+++ Scripts/Logging.cs
@@ -0,0 +1,56 @@
+using System.Linq;
+
+namespace ProceduralCaves
+{
+    public enum LoggingLevel
+    {
+        DEBUG,
+        INFO,
+        WARNING,
+        ERROR,
+    }
+
+    public class Log
+    {
+        public static LoggingLevel loggingLevel = LoggingLevel.DEBUG;
+
+        public static string loggerName = "PoweredFlashlights";
+
+        private static string ObjectsToString(object[] objects)
+        {
+            return string.Join(" ", objects.Select(obj => obj.ToString()));
+        }
+
+        public static void Debug(params object[] objects)
+        {
+            if (loggingLevel < LoggingLevel.DEBUG)
+                return;
+
+            global::Log.Out($"[{loggerName}] {ObjectsToString(objects)}");
+        }
+
+        public static void Info(params object[] objects)
+        {
+            if (loggingLevel < LoggingLevel.INFO)
+                return;
+
+            global::Log.Out($"[{loggerName}] {ObjectsToString(objects)}");
+        }
+
+        public static void Warning(params object[] objects)
+        {
+            if (loggingLevel < LoggingLevel.WARNING)
+                return;
+
+            global::Log.Warning($"[{loggerName}] {ObjectsToString(objects)}");
+        }
+
+        public static void Error(params object[] objects)
+        {
+            if (loggingLevel < LoggingLevel.ERROR)
+                return;
+
+            global::Log.Error($"[{loggerName}] {ObjectsToString(objects)}");
+        }
+    }
+}
