2554cf298e862218dd0eea14f53e34adb4256529
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2024-11-04 20:17:04 +0100

    setup EAIEatBlock class and make it run ingame

--- Scripts/EAI/EAIEatBlock.cs
+++ Scripts/EAI/EAIEatBlock.cs
@@ -1,8 +1,18 @@
 using System.Collections.Generic;
-using UnityEngine;
 
-public class EAIBreakBlock : EAIBase
+public class EAIEatBlock : EAIBase
 {
+    private struct BlockTargetData
+    {
+        public static readonly BlockTargetData Null = default;
+
+        public BlockValue blockValue;
+
+        public Vector3i Position;
+
+        public string BlockName => blockValue.Block.blockName;
+    }
+
     public const float cDamageBoostPerAlly = 0.2f;
 
     public int attackDelay;
@@ -11,6 +21,12 @@ public class EAIBreakBlock : EAIBase
 
     public List<Entity> allies = new List<Entity>();
 
+    private BlockTargetData blockTargetDatas;
+
+    private readonly int searchRadius = 10;
+
+    private List<BlockValue> targetClasses;
+
     public override void Init(EntityAlive _theEntity)
     {
         base.Init(_theEntity);
@@ -18,55 +34,64 @@ public class EAIBreakBlock : EAIBase
         executeDelay = 0.15f;
     }
 
+    public override void SetData(DictionarySave<string, string> data)
+    {
+        base.SetData(data);
+
+        targetClasses = new List<BlockValue>();
+
+        if (!data.TryGetValue("class", out var _value))
+        {
+            return;
+        }
+    }
+
     public override bool CanExecute()
     {
-        EntityMoveHelper moveHelper = theEntity.moveHelper;
-        if ((theEntity.Jumping && !moveHelper.IsDestroyArea) || theEntity.bodyDamage.CurrentStun != 0)
+
+        if (theEntity.sleepingOrWakingUp || theEntity.bodyDamage.CurrentStun != 0 || (theEntity.Jumping && !theEntity.isSwimming))
         {
+            Log.Out($"[Cave] EAIEatBlock: Can't Execute");
             return false;
         }
-        if (moveHelper.BlockedTime > 0.35f && moveHelper.CanBreakBlocks)
+
+        blockTargetDatas = FindBlockToEat(searchRadius);
+        if (blockTargetDatas.Position == Vector3i.zero)
         {
-            if (moveHelper.HitInfo != null)
-            {
-                Vector3i blockPos = moveHelper.HitInfo.hit.blockPos;
-                if (theEntity.world.GetBlock(blockPos).isair)
-                {
-                    return false;
-                }
-            }
-            float num = moveHelper.CalcBlockedDistanceSq();
-            float num2 = theEntity.m_characterController.GetRadius() + 0.6f;
-            if (num <= num2 * num2)
-            {
-                return true;
-            }
+            Log.Out($"[Cave] EAIEatBlock: no block to eat found");
+            return false;
         }
+
+        // Type type = blockTargetPosition.GetType();
+        // for (int i = 0; i < targetClasses.Count; i++)
+        // {
+        //     TargetClass targetClass = targetClasses[i];
+        //     if (targetClass.type != null && targetClass.type.IsAssignableFrom(type))
+        //     {
+        //         chaseTimeMax = targetClass.chaseTimeMax;
+        //         return true;
+        //     }
+        // }
         return false;
     }
 
-    public override void Start()
-    {
-        attackDelay = 1;
-        Vector3i blockPos = theEntity.moveHelper.HitInfo.hit.blockPos;
-        Block block = theEntity.world.GetBlock(blockPos).Block;
-        if (block.HasTag(BlockTags.Door) || block.HasTag(BlockTags.ClosetDoor))
-        {
-            theEntity.IsBreakingDoors = true;
-        }
-    }
+    // public override void Start()
+    // {
+    //     attackDelay = 1;
+    //     Vector3i blockPos = theEntity.moveHelper.HitInfo.hit.blockPos;
+    //     Block block = theEntity.world.GetBlock(blockPos).Block;
+    //     if (block.HasTag(BlockTags.Door) || block.HasTag(BlockTags.ClosetDoor))
+    //     {
+    //         theEntity.IsBreakingDoors = true;
+    //     }
+    // }
 
     public override bool Continue()
     {
-        if (theEntity.bodyDamage.CurrentStun != 0)
-        {
+        if (theEntity.bodyDamage.CurrentStun != 0 || !theEntity.onGround)
             return false;
-        }
-        if (theEntity.onGround)
-        {
-            return CanExecute();
-        }
-        return false;
+
+        return CanExecute();
     }
 
     public override void Update()
@@ -84,34 +109,64 @@ public class EAIBreakBlock : EAIBase
 
     public override void Reset()
     {
+        theEntity.IsEating = false;
         theEntity.IsBreakingBlocks = false;
         theEntity.IsBreakingDoors = false;
     }
 
     public void AttackBlock()
     {
-        theEntity.SetLookPosition(Vector3.zero);
-        if (!(theEntity.inventory.holdingItemData.actionData[0] is ItemActionAttackData itemActionAttackData))
+        var targetPos = blockTargetDatas.Position;
+
+        theEntity.SetLookPosition(targetPos);
+        theEntity.moveHelper.SetMoveTo(targetPos, _canBreakBlocks: false);
+
+        Log.Out($"[Cave] move entity '{theEntity.entityId}' to {blockTargetDatas.Position} ({blockTargetDatas.blockValue})");
+
+        /* if (!(theEntity.inventory.holdingItemData.actionData[0] is ItemActionAttackData itemActionAttackData))
         {
             return;
         }
-        damageBoostPercent = 0f;
 
-        if (theEntity is EntityZombie)
+        if (theEntity.Climbing)
         {
-            Bounds bb = new Bounds(theEntity.position, new Vector3(1.7f, 1.5f, 1.7f));
-            theEntity.world.GetEntitiesInBounds(typeof(EntityZombie), bb, allies);
-            for (int num = allies.Count - 1; num >= 0; num--)
-            {
-                if ((EntityZombie)allies[num] != theEntity)
-                {
-                    damageBoostPercent += 0.2f;
-                }
-            }
-            allies.Clear();
+            return;
         }
+
+        if (theEntity.IsEating)
+        {
+            if (theEntity.bodyDamage.HasLimbs)
+            {
+                theEntity.RotateTo(vector2.x, vector2.y, vector2.z, 8f, 5f);
+            }
+            if (attackTimeout <= 0)
+            {
+                attackTimeout = 25 + GetRandom(10);
+                if ((eatCount++ & 1) == 0)
+                {
+                    theEntity.PlayOneShot("eat_player");
+                    blockTargetPosition.DamageEntity(DamageSource.eat, 35, _criticalHit: false);
+                }
+                Vector3 pos = new Vector3(0f, 0.04f, 0.08f);
+                ParticleEffect pe = new ParticleEffect("blood_eat", pos, 1f, Color.white, null, theEntity.entityId, ParticleEffect.Attachment.Head);
+                GameManager.Instance.SpawnParticleEffectServer(pe, theEntity.entityId);
+            }
+
+            theEntity.moveHelper.SetMoveTo(blockTargetPosition, _canBreakBlocks: false);
+
+            if (isTargetToEat)
+            {
+                isEating = true;
+                theEntity.IsEating = true;
+                attackTimeout = 20;
+                eatCount = 0;
+                return;
+            }
+        }
+
         if (theEntity.Attack(_bAttackReleased: false))
         {
+            theEntity.IsEating = true;
             theEntity.IsBreakingBlocks = true;
             float num2 = 0.25f + base.RandomFloat * 0.8f;
             if (theEntity.moveHelper.IsUnreachableAbove)
@@ -121,13 +176,11 @@ public class EAIBreakBlock : EAIBase
             attackDelay = (int)((num2 + 0.75f) * 20f);
             itemActionAttackData.hitDelegate = GetHitInfo;
             theEntity.Attack(_bAttackReleased: true);
-        }
+        } */
     }
 
-    public WorldRayHitInfo GetHitInfo(out float damageScale)
+    private BlockTargetData FindBlockToEat(int radius)
     {
-        EntityMoveHelper moveHelper = theEntity.moveHelper;
-        damageScale = moveHelper.DamageScale + damageBoostPercent;
-        return moveHelper.HitInfo;
+        return BlockTargetData.Null;
     }
 }
