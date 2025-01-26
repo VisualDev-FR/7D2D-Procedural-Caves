b2ff5ae967b6ba739080b2d3626199652181e1e1
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2025-01-26 16:52:12 +0100

    improve helpers.py

--- helpers.py
+++ helpers.py
@@ -1,12 +1,52 @@
 from __future__ import annotations
 
-from pathlib import Path
+from zipfile import ZipFile
 from typing import Callable
+from pathlib import Path
 import subprocess
 import argparse
-import zipfile
 import shutil
+import json
 import os
+
+
+class BuildInfos:
+
+    def __init__(self, dir: Path):
+
+        with open(Path(dir, "build.json"), "rb") as reader:
+            datas = json.load(reader)
+
+        self.name = datas["name"]
+        self.csproj = Path(datas["csproj"])
+        self.include = [Path(dir, path) for path in datas["include"]]
+        self.dependencies = [Path(dir, path) for path in datas["dependencies"]]
+
+
+class CommandLineInterface:
+
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        subparsers = parser.add_subparsers()
+
+        # fmt: off
+        self._add_subcommand(subparsers, "compile", compile, "Compile the project and create a zip archive ready for testing")
+        self._add_subcommand(subparsers, "release", release, "Compile the project and create the release zip archive")
+        self._add_subcommand(subparsers, "shut-down", shut_down, "Close all instances of the game")
+        self._add_subcommand(subparsers, "start-local", start_local, "Compile the project, then start a local game")
+        # fmt: on
+
+        args = parser.parse_args()
+
+        if hasattr(args, "func"):
+            args.func()
+        else:
+            parser.print_help()
+
+    def _add_subcommand(self, subparsers, name: str, func: Callable, help_text: str):
+        """Ajoute une sous-commande à l'argument parser"""
+        subcommand_parser = subparsers.add_parser(name, help=help_text)
+        subcommand_parser.set_defaults(func=func)
 
 
 MOD_NAME = "TheDescent"
@@ -14,9 +54,11 @@ PATH_7D2D = Path(os.environ["PATH_7D2D"])
 PATH_7D2D_USER = Path(os.environ["APPDATA"], "7DaysToDie")
 PATH_7D2D_EXE = Path(PATH_7D2D, "7DaysToDie.exe")
 MOD_PATH = Path(PATH_7D2D, "Mods", MOD_NAME)
+PREFABS_PATH = Path(PATH_7D2D_USER, "LocalPrefabs/leopard/Prefabs")
 
 BUILD_ZIP = Path(f"{MOD_NAME}.zip")
-BUILD_DIR = Path(MOD_NAME)
+BUILD_DIR = Path("build")
+BUILD_INFOS = BuildInfos("")
 
 
 def _return_code(command: str) -> int:
@@ -40,9 +82,9 @@ def _include_dir(dir: Path):
     shutil.copytree(dir, Path(BUILD_DIR, dir))
 
 
-def compile():
+def compile(buildInfos: BuildInfos = BUILD_INFOS):
 
-    if _return_code("dotnet build --no-incremental") != 0:
+    if _return_code(f"dotnet build --no-incremental {buildInfos.csproj.absolute()}") != 0:
         raise SystemExit()
 
     if BUILD_ZIP.exists():
@@ -61,14 +103,13 @@ def compile():
     _include_file("ModInfo.xml")
 
     shutil.make_archive(MOD_NAME, "zip", BUILD_DIR)
-    shutil.rmtree(BUILD_DIR)
 
 
 def install():
     if MOD_PATH.exists():
         shutil.rmtree(MOD_PATH)
 
-    with zipfile.ZipFile(BUILD_ZIP, "r") as zip_file:
+    with ZipFile(BUILD_ZIP, "r") as zip_file:
         zip_file.extractall(MOD_PATH)
 
 
@@ -108,29 +149,28 @@ def clear_world(world_name: str, save_name: str = "Caves", hard: bool = False):
         shutil.rmtree(world_dir)
 
 
-class CommandLineInterface:
+def release():
+    """
+    TODOC
+    """
+    compile()
 
-    def __init__(self):
-        parser = argparse.ArgumentParser()
-        subparsers = parser.add_subparsers()
+    shutil.rmtree(BUILD_DIR, ignore_errors=True)
+    os.makedirs(BUILD_DIR)
 
-        # fmt: off
-        self._add_subcommand(subparsers, "compile", compile, "Compile the project and create a zip archive ready for testing")
-        self._add_subcommand(subparsers, "shut-down", shut_down, "Close all instances of the game")
-        self._add_subcommand(subparsers, "start-local", start_local, "Compile the project, then start a local game")
-        # fmt: on
+    for path in DEPENDENCIES + [BUILD_ZIP]:
+        if not path.exists():
+            raise SystemExit(f"Missing dependency: '{path}'")
 
-        args = parser.parse_args()
+        if not path.is_file():
+            raise SystemExit(f"Dependency is not a file: '{path}'")
 
-        if hasattr(args, "func"):
-            args.func()
-        else:
-            parser.print_help()
+        dst = Path(BUILD_DIR, path.stem)
 
-    def _add_subcommand(self, subparsers, name: str, func: Callable, help_text: str):
-        """Ajoute une sous-commande à l'argument parser"""
-        subcommand_parser = subparsers.add_parser(name, help=help_text)
-        subcommand_parser.set_defaults(func=func)
+        with ZipFile(path, "r") as zip_file:
+            zip_file.extractall(dst)
+
+    shutil.make_archive(f"{MOD_NAME}-release", "zip", BUILD_DIR)
 
 
 if __name__ == "__main__":
