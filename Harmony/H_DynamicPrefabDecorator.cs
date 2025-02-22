6fe23ea1e232dfdbac6174e30a63f63634aa664d
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2025-02-22 19:44:44 +0100

    investigate next_traders_quests

--- Harmony/H_DynamicPrefabDecorator.cs
+++ Harmony/H_DynamicPrefabDecorator.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using HarmonyLib;
+using UnityEngine;
 
 
 [HarmonyPatch(typeof(DynamicPrefabDecorator), "DecorateChunk")]
@@ -12,5 +14,20 @@ public class DynamicPrefabDecorator_DecorateChunk
         {
             CaveGenerator.GenerateCaveChunk(_chunk);
         }
+    }
+}
+
+
+[HarmonyPatch(typeof(DynamicPrefabDecorator), "GetClosestPOIToWorldPos")]
+public class DynamicPrefabDecorator_GetClosestPOIToWorldPos
+{
+    // prevents traders to offer quest to go at underground trader
+    public static bool Prefix(FastTags<TagGroup.Global> questTag, Vector2 worldPos, List<Vector2> excludeList = null, int maxSearchDist = -1, bool ignoreCurrentPOI = false, BiomeFilterTypes biomeFilterType = BiomeFilterTypes.SameBiome, string biomeFilter = "")
+    {
+        var logger = Logging.CreateLogger("H_DynamicPrefabDecorator.GetClosestPOIToWorldPos");
+
+        logger.Debug(questTag.ToString());
+
+        return true;
     }
 }
