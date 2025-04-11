691d66ad0f43f2492004dd9188585a1d4909cebb
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2025-04-11 17:45:24 +0200

    code cleaning, no more need to patch ChunkProviderGenerateWorld.Init

--- Harmony/H_ChunkProviderGenerateWorld.cs
+++ Harmony/H_ChunkProviderGenerateWorld.cs
@@ -1,36 +1,12 @@
-using System.Collections;
 using HarmonyLib;
 
 
 [HarmonyPatch(typeof(ChunkProviderGenerateWorld), "Init")]
 public static class ChunkProviderGenerateWorld_Init
 {
-    public static bool Prefix(ChunkProviderGenerateWorld __instance, World _world, ref IEnumerator __result)
+    public static bool Prefix(World _world)
     {
-        __result = Init(__instance, _world);
-        return false;
-    }
-
-    private static IEnumerator Init(ChunkProviderGenerateWorld __instance, World _world)
-    {
-        var worldLocation = PathAbstractions.WorldsSearchPaths.GetLocation(__instance.levelName);
-
-        __instance.prefabDecorator = new DynamicPrefabDecorator();
-        __instance.spawnPointManager = new SpawnPointManager();
-        __instance.world = _world;
-
-        yield return __instance.prefabDecorator.Load(worldLocation.FullPath);
-
-        // PATCH START
         CaveGenerator.Init();
-        // PATCH END
-
-        if (!__instance.bClientMode)
-        {
-            __instance.spawnPointManager.Load(worldLocation.FullPath);
-            __instance.threadInfo = ThreadManager.StartThread("GenerateChunks", null, __instance.GenerateChunksThread, null, System.Threading.ThreadPriority.Lowest, null, null, _useRealThread: true);
-        }
-
-        yield return null;
+        return true;
     }
 }
